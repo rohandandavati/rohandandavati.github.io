999259d0fd013efba536ad364b89ef797c114ea6
Rohan Dandavati <rohandandavati@gmail.com>
2020-08-16 15:50:53 -0700

    plot with real R_t estimates

--- graphs.js
+++ graphs.js
@@ -14,11 +14,8 @@ var svg_conf = d3.select("#conf-interval")
 
 //Read data
 //need to spin up server to read locally 
-//d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_IC.csv",function(data) {
- d3.csv("file.csv",function(data) {
-
-
-
+ d3.csv("rtData.csv", function(data) {
+    console.log(data);
     // Add X axis
     var x = d3.scaleLinear()
       .domain([1,100])
@@ -29,7 +26,7 @@ var svg_conf = d3.select("#conf-interval")
 
     // Add Y axis
     var y = d3.scaleLinear()
-      .domain([0, 13])
+      .domain([0, 7])
       .range([ height, 0 ]);
     svg_conf.append("g")
       .call(d3.axisLeft(y));
@@ -40,9 +37,9 @@ var svg_conf = d3.select("#conf-interval")
       .attr("fill", "#cce5df")
       .attr("stroke", "none")
       .attr("d", d3.area()
-        .x(function(d) { return x(d.x) })
-        .y0(function(d) { return y(d.CI_right) })
-        .y1(function(d) { return y(d.CI_left) })
+        .x(function(d) { return x(d.time) })
+        .y0(function(d) { return y(d.rt_lower) })
+        .y1(function(d) { return y(d.rt_upper) })
         )
 
     // Add the line
@@ -53,117 +50,153 @@ var svg_conf = d3.select("#conf-interval")
       .attr("stroke", "steelblue")
       .attr("stroke-width", 1.5)
       .attr("d", d3.line()
-        .x(function(d) { return x(d.x) })
-        .y(function(d) { return y(d.y) })
+        .x(function(d) { return x(d.time) })
+        .y(function(d) { return y(d.rt_mean) })
         )
-
+  console.log("attempting to plot");
 })
+
+
 /*
-// set the dimensions and margins of the graph
-var margin_cases = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 460 - margin_cases.left - margin_cases.right,
-    height = 400 - margin_cases.top - margin_cases.bottom;
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
 
-// append the svg object to the body of the page
-var svg_cases = d3.select("#cases")
-  .append("svg")
-    .attr("width", width + margin_cases.left + margin_cases.right)
-    .attr("height", height + margin_cases.top + margin_cases.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin_cases.left + "," + margin_cases.top + ")");
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
 
-// get the data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", function(data) {
-
-  // X axis: scale and draw:
-  var x = d3.scaleLinear()
-      .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-      .range([0, width]);
-  svg_cases.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-  // set the parameters for the histogram
-  var histogram = d3.histogram()
-      .value(function(d) { return d.price; })   // I need to give the vector of value
-      .domain(x.domain())  // then the domain of the graphic
-      .thresholds(x.ticks(70)); // then the numbers of bins
-
-  // And apply this function to data to get the bins
-  var bins = histogram(data);
-
-  // Y axis: scale and draw:
-  var y = d3.scaleLinear()
-      .range([height, 0]);
-      y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
-  svg_cases.append("g")
-      .call(d3.axisLeft(y));
-
-  // append the bar rectangles to the svg element
-  svg_cases.selectAll("rect")
-      .data(bins)
-      .enter()
-      .append("rect")
-        .attr("x", 1)
-        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-        .attr("height", function(d) { return height - y(d.length); })
-        .style("fill", "#69b3a2")
-
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  authorize(JSON.parse(content), listRows);
 });
 
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ 
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
 
-// set the dimensions and margins of the graph
-var margin_deaths = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 460 - margin_deaths.left - margin_deaths.right,
-    height = 400 - margin_deaths.top - margin_deaths.bottom;
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
 
-// append the svg object to the body of the page
-var svg_deaths = d3.select("#deaths")
-  .append("svg")
-    .attr("width", width + margin_deaths.left + margin_deaths.right)
-    .attr("height", height + margin_deaths.top + margin_deaths.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin_deaths.left + "," + margin_deaths.top + ")");
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ 
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error while trying to retrieve access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
 
-// get the data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", function(data) {
+/**
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ 
+function listRows(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  sheets.spreadsheets.values.get({
+    spreadsheetId: '1Vpn0bVPvSypRLM_uxpE9Zp_6Ft_Hg09b7r5CLtviswg',
+    range: 'A1:D101',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const data = res.data.values;
+    if (data.length) {
+      console.log(data);
+      // set dimensions margins of graph
+      var margin_conf = {top: 10, right: 10, bottom: 30, left: 60},
+      width = 660 - margin_conf.left - margin_conf.right,
+      height = 400 - margin_conf.top - margin_conf.bottom;
 
-  // X axis: scale and draw:
-  var x = d3.scaleLinear()
-      .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-      .range([0, width]);
-  svg_deaths.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      // append svg object to body
+      var svg_conf = d3.select("#conf-interval")
+      .append("svg")
+      .attr("width", width + margin_conf.left + margin_conf.right)
+      .attr("height", height + margin_conf.top + margin_conf.bottom)
+      .append("g")
+      .attr("transform",
+            "translate(" + margin_conf.left + "," + margin_conf.top + ")");
 
-  // set the parameters for the histogram
-  var histogram = d3.histogram()
-      .value(function(d) { return d.price; })   // I need to give the vector of value
-      .domain(x.domain())  // then the domain of the graphic
-      .thresholds(x.ticks(70)); // then the numbers of bins
+      //Read data
+      //need to spin up server to read locally 
+      //d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_IC.csv",function(data) {
+      // d3.csv("file.csv",function(data) {
+      // Add X axis
+      var x = d3.scaleLinear()
+        .domain([1,100])
+        .range([ 0, width ]);
+      svg_conf.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
-  // And apply this function to data to get the bins
-  var bins = histogram(data);
+      // Add Y axis
+      var y = d3.scaleLinear()
+        .domain([0, 13])
+        .range([ height, 0 ]);
+      svg_conf.append("g")
+        .call(d3.axisLeft(y));
 
-  // Y axis: scale and draw:
-  var y = d3.scaleLinear()
-      .range([height, 0]);
-      y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
-  svg_deaths.append("g")
-      .call(d3.axisLeft(y));
+      // Show confidence interval
+      svg_conf.append("path")
+        .datum(data)
+        .attr("fill", "#cce5df")
+        .attr("stroke", "none")
+        .attr("d", d3.area()
+          .x(function(d) { return x(d.x) })
+          .y0(function(d) { return y(d.CI_right) })
+          .y1(function(d) { return y(d.CI_left) })
+          )
 
-  // append the bar rectangles to the svg element
-  svg_deaths.selectAll("rect")
-      .data(bins)
-      .enter()
-      .append("rect")
-        .attr("x", 1)
-        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-        .attr("height", function(d) { return height - y(d.length); })
-        .style("fill", "#69b3a2")
-
-});*/
+      // Add the line
+      svg_conf
+        .append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+          .x(function(d) { return x(d.x) })
+          .y(function(d) { return y(d.y) })
+          )
+    } else {
+      console.log('No data found.');
+    }
+  });
+}
+*/
